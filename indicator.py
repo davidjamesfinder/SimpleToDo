eb68032269f1fca923db6fa43ab7d52ac8bb4a0e
David <davidjfinder@gmail.com>
2017-01-29 21:40:24 -0500

    Adding Categories

--- indicator.py
+++ indicator.py
@@ -19,24 +19,36 @@ class AddItemPopup(Gtk.Window): #We need this to be destructable, so we make it 
         self.set_border_width(20) #Set border width to 20
         #Add an icon?
         self.connect('delete-event', self.quit) #Connect deletion field
-        self.vbox = Gtk.VBox(spacing=6)
-        self.add(self.vbox)
-        self.boxone = Gtk.HBox(spacing=6) #Use a horizontal box. I probably could try to use a grid instead
-        self.add(self.box) #Add box
-        #self.label = Gtk.Label("Add A New Item") #Honestly, takes up too much space and is repetitive
-        self.entryField = Gtk.Entry() #Needs to be an attribute, because we reference it later.
-        submitbutton = Gtk.Button(label="Add", stock=None) #Add a rather generic button
 
-        submitbutton.connect("clicked", indicator.AddItem, self) #Connect it to the add item function on indicator
+        self.table = Gtk.Grid()
+        #self.table 
+        self.add(self.table)
+
+        #self.vbox = Gtk.VBox(spacing=6)
+        #self.add(self.vbox)
+        #self.boxone = Gtk.HBox(spacing=6) #Use a horizontal box. I probably could try to use a grid instead
+
+        self.nameLabel = Gtk.Label("Name") #Honestly, takes up too much space and is repetitive
+        self.nameField = Gtk.Entry() #Needs to be an attribute, because we reference it later.
+        self.categoryLabel = Gtk.Label("Category")
+        self.categoryField = Gtk.Entry()
+        self.submitButton = Gtk.Button(label="Add", stock=None) #Add a rather generic button
+        self.submitButton.connect("clicked", indicator.AddItem, self) #Connect it to the add item function on indicator
+        self.table.attach(self.nameLabel,0,0,1,1)
+        self.table.attach(self.nameField,1,0,2,1)
+        self.table.attach(self.categoryLabel,0,1,1,1)
+        self.table.attach(self.categoryField,1,1,2,1)
+        self.table.attach(self.submitButton,0,2,3,1)
+        self.table.set_column_spacing(6)
         #self.box.pack_start(self.label, False, True, 0)
-        self.box.pack_start(self.entryField, True, True, 0) #Pack in the entry field
-        self.box.pack_start(submitbutton, False, True, 0) #Pack in the submit button
+        #self.boxone.pack_start(self.entryField, True, True, 0) #Pack in the entry field
+        #self.boxone.pack_start(submitbutton, False, True, 0) #Pack in the submit button
         self.show_all() #Showtime
  
     def quit(self,window,connection):
             Gtk.Widget.destroy(self) #Destroy self. This is necessary so we don't quit the application as a whole
  
-class AppIndicator(object):
+class ToDoIndicator(object):
         def __init__(self):
                 self.ind = AppIndicator.Indicator.new("Reminder Application",
                     'distributor-logo', AppIndicator.IndicatorCategory.APPLICATION_STATUS)
@@ -44,12 +56,13 @@ class AppIndicator(object):
                 #self.label=AppIndicator.Indicator.new_label("Testing")
                 self.ind.set_label("Tikkun Atzmi","Tikkun Atzmi") 
                 if not os.path.exists('data.json'):
-                        file('data.json','w+') #(If the file doesn't exist, initialize it)
-                file_attributes=os.stat('./data.json') #Read the statistics for our data file
+                        file(storageLocation,'w+') #(If the file doesn't exist, initialize it)
+                file_attributes=os.stat(storageLocation) #Read the statistics for our data file
                 self.itemList = [] #Initialize local copy of the item list
+                #print os.system("ls")
                 if file_attributes.st_size > 0: #If the file has at least one line... Otherwise, attempting to read lines throws an error
                         #For whatever reason, there isn't a good way to essentially unfreeze objects from a data-store. Pickling doesn't work with objects, JSONPickle doesn't unformat properly, but does provide easily readable data
-                        self.file = open('./data.json', 'r') #Read the file, read only.
+                        self.file = open(storageLocation, 'r') #Read the file, read only.
                         for line in self.file: #For each of the lines...
                                 print line
                                 if not line.isspace(): #If we don't have a single line
@@ -59,8 +72,8 @@ class AppIndicator(object):
                                                 #print myLine.__dict__
                                                 #myLine.category = None
 
-                                                currentItem = jsonpickle.decode(line)
-                                                self.itemList.append(currentItem) #Append this item to our item list
+                                    currentItem = jsonpickle.decode(line)
+                                    self.itemList.append(currentItem) #Append this item to our item list
                                                
                         self.file.close()
                 self.render() #Render
@@ -116,9 +129,9 @@ class AppIndicator(object):
  
         def AddItem(indicator,button,myWindow):
 
-                myItem = Item(myWindow.entryField.get_text())#Create an item
+                myItem = Item(myWindow.nameField.get_text(),myWindow.categoryField.get_text())#Create an item
                 indicator.itemList.append(myItem)#Add item to local list
-                myFile = open('data.json', 'a+')#Write to the storage file starting at the end.
+                myFile = open(storageLocation, 'a+')#Write to the storage file starting at the end.
                 #print json.dump(myItem.__dict__)
                 encodement=jsonpickle.encode(myItem)#Encode Item object as a line.
                 myFile.write("\n")#Seperate lines. Putting this line after the next one is ugly because when we delete whitespace, it would remove the useful whitespace
@@ -130,7 +143,7 @@ class Item(object):
         def __init__(self, name, category=None):
             self.id = uuid.uuid1()
             self.name = name
-            self.category = None
+            self.category = category
             #self.category = category
         #def set_item_id(self):
         #    myFile=open('data.json','a+') #Delete the data, so that we can conveniently rewrite it
@@ -140,10 +153,10 @@ class Item(object):
         #    myFile.write("\n")       
         #    myFile.close()
         def delete(self,connection,indicator):
-                myFile = open('data.json', 'r')
+                myFile = open(storageLocation, 'r')
                 lines=myFile.readlines() #Read all the lines from our file
                 myFile.close() #Is this necessary?
-                myFile=open('data.json','w') #Delete the data, so that we can conveniently rewrite it
+                myFile=open(storageLocation,'w') #Delete the data, so that we can conveniently rewrite it
                 for line in lines: #For each of the lines in the file
                         if self.id.hex not in line: #If the name of our item isn't in the line, don't write
                             if line != '\n': #Don't write extraneous lines. These mainly occur if we delete a line in the middle.
@@ -153,7 +166,10 @@ class Item(object):
                 indicator.render() #Re-render the applet, now that we have altered the view
 
  
-indicator = AppIndicator()
+global storageLocation
+theActualPath = os.path.dirname(os.path.realpath(__file__))
+storageLocation = theActualPath + "/data.json"
+indicator = ToDoIndicator()
 #thread.start_new_thread #For when I get around to implimenting threading
 
 Gtk.main()
